ae80278e9e72349b67a261f5616a82ed901c9e3d
Ionic Bond <scifightersrobotics@gmail.com>
2025-10-30 20:28:42 +0200

    Improved about page change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -123,54 +123,58 @@ export default function HomePage() {
           </div>
         </div>
         <div className="mx-auto max-w-screen-xl px-4">
-          <div className="hero-content flex flex-col lg:flex-row items-center gap-8 relative lg:left-10 py-10 text-center">
+          <div className="hero-content flex flex-col lg:flex-row items-center gap-10 relative lg:left-10 py-10 text-center">
             {/* Logo */}
             <Carousel
                 images={aboutImages}
                 interval={3000}
                 autoScroll
-                className="w-[90vw] max-w-[1000px] h-[240px] sm:h-[280px] md:h-[320px] lg:h-[400px] mr-2 ml-2 mt-10 mb-10"
+                className="w-full max-w-[600px] rounded-lg shadow-2xl min-h-[300px] sm:min-h-[400px]"
              />
 
             {/* Text and title */}
-            <div className="px-4 max-w-2xl text-center flex flex-col items-center gap-4">
+            <div className="px-4 max-w-xl text-center flex flex-col items-center gap-4">
               <h1 className="text-2xl sm:text-5xl text-team font-bold font-anton leading-tight tracking-wide">
                 Who We Are
               </h1>
-              <p className="text-lg sm:text-base lg:text-xl py-4">
-
-                We are{" "}
-                <span className="italic font-bold text-team">
+                <div className="mx-auto max-w-screen-2xl px-4 flex flex-wrap justify-center gap-4">
+                    <p className="text-base sm:text-lg">
+                        We are{" "}
+                        <span className="italic font-bold text-team">
                   #9738 Ionic Bond
                 </span>
-                , a FIRST Robotics Competition team from Tel-Mond, Israel. Our
-                team, which was established in 2024, consists of 17 passionate
-                high-school students, and 7 dedicated volunteer mentors.<br />
-
-                <br />During the 2024 CRESCENDO season, we made a remarkable debut,
-                proudly serving as the
-                <strong> alliance captain for the 4th alliance</strong> in
-                Israeli District #4. We are also proud to have received the{" "}
-                <strong>Rookie Inspiration Award</strong> (given to first-year
-                teams that had a great impact on their community) twice and the{" "}
-                <strong>Rookie High Seed Award</strong> (given to first-year
-                teams that has made the highest ranking out of the rookie teams)
-                three times.<br />                                          <br />
-                Alongside our competitive activities, we participate in outreach
-                initiatives to make an impact on our community and encourage a
-                passion for{" "}
-                <span className="relative group cursor-pointer font-bold">
+                        , a FIRST Robotics Competition team from Tel-Mond, Israel. Our
+                        team, which was established in 2024, consists of 17 passionate
+                        high-school students, and 7 dedicated volunteer mentors.
+                    </p>
+                    <p className="sm:text-lg">
+                        During the 2024 CRESCENDO season, we made a remarkable debut,
+                        proudly serving as the
+                        <strong> alliance captain for the 4th alliance</strong> in
+                        Israeli District #4. We are also proud to have received the{" "}
+                        <strong>Rookie Inspiration Award</strong> (given to first-year
+                        teams that had a great impact on their community) twice and the{" "}
+                        <strong>Rookie High Seed Award</strong> (given to first-year
+                        teams that has made the highest ranking out of the rookie teams)
+                        three times.
+                    </p>
+                    <p className="sm:text-lg">
+                        Alongside our competitive activities, we participate in outreach
+                        initiatives to make an impact on our community and encourage a
+                        passion for{" "}
+                        <span className="relative group cursor-pointer font-bold">
                   STEAM
                   <span
-                    className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2
+                      className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2
                px-2 py-1 text-sm text-white bg-gray-800 rounded opacity-0
                group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap"
                   >
                     Science, Technology, Engineering, Arts, and Math
                   </span>
                 </span>
-                .
-              </p>
+                        .
+                    </p>
+                </div>
 
               <button className="btn btn-sm sm:btn-md md:btn-lg lg:btn-xl btn-team text-xl text-zinc-900 px-30 py-5">
                   <a href={"/impact"}>Details</a>
