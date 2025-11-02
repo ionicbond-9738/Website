a92da86a643d8d8ee8e2f613d6b736a96a5ea2d1
Ionic Bond <scifightersrobotics@gmail.com>
2025-11-02 21:08:03 +0200

    format

--- next.config.ts
+++ next.config.ts
@@ -3,40 +3,40 @@ import type { NextConfig } from "next";
 const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
 
 const nextConfig: NextConfig = {
-    output: "export", // ✅ required for static export
-    images: {
-        unoptimized: true, // ✅ disable optimization for static export
-    },
-    basePath: repoName ? `/${repoName}` : "",
-    assetPrefix: repoName ? `/${repoName}/` : "",
-    webpack(config) {
-        // Grab the existing rule that handles SVG imports
-        const fileLoaderRule = config.module.rules.find((rule: any) =>
-            rule.test?.test?.(".svg")
-        );
+  output: "export", // ✅ required for static export
+  images: {
+    unoptimized: true, // ✅ disable optimization for static export
+  },
+  basePath: repoName ? `/${repoName}` : "",
+  assetPrefix: repoName ? `/${repoName}/` : "",
+  webpack(config) {
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule: any) =>
+      rule.test?.test?.(".svg"),
+    );
 
-        if (!fileLoaderRule) return config;
+    if (!fileLoaderRule) return config;
 
-        // Reapply the existing rule, but only for svg imports ending in ?url
-        config.module.rules.push(
-            {
-                ...fileLoaderRule,
-                test: /\.svg$/i,
-                resourceQuery: /url/, // *.svg?url
-            },
-            {
-                test: /\.svg$/i,
-                issuer: fileLoaderRule.issuer,
-                resourceQuery: { not: [/url/] }, // all other *.svg imports
-                use: ["@svgr/webpack"], // use SVGR for React components
-            }
-        );
+    // Reapply the existing rule, but only for svg imports ending in ?url
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [/url/] }, // all other *.svg imports
+        use: ["@svgr/webpack"], // use SVGR for React components
+      },
+    );
 
-        // Modify the original file loader to ignore *.svg
-        fileLoaderRule.exclude = /\.svg$/i;
+    // Modify the original file loader to ignore *.svg
+    fileLoaderRule.exclude = /\.svg$/i;
 
-        return config;
-    },
+    return config;
+  },
 };
 
 export default nextConfig;
