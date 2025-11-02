6b36f506414862470aef429ef4eb7eb22cafd8b8
Ionic Bond <scifightersrobotics@gmail.com>
2025-11-02 20:24:29 +0200

    format

--- src/app/campaigns/parents-info/page.tsx
+++ src/app/campaigns/parents-info/page.tsx
@@ -3,21 +3,21 @@ import Footer from "@/components/Footer";
 import Image from "next/image";
 
 export default function Support() {
-    return (
-        <div>
-            <div className="bg-black text-white min-h-screen flex flex-col">
-                <Navbar />
-                <main className="flex-grow mt-5 mx-auto">
-                    <Image
-                        src={"/assets/misc/campaigns/parents_info.png"}
-                        alt={"Parents Information"}
-                        width={600}
-                        height={600}
-                        className="w-full h-auto object-contain rounded-lg"
-                    />
-                </main>
-                <Footer />
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="bg-black text-white min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-grow mt-5 mx-auto">
+          <Image
+            src={"/assets/misc/campaigns/parents_info.png"}
+            alt={"Parents Information"}
+            width={600}
+            height={600}
+            className="w-full h-auto object-contain rounded-lg"
+          />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  );
 }
